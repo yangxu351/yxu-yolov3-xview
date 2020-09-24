bff9f545234655271568ec8292f5822bd9e6371a
yang.xu <1554404832@qq.com>
2020-09-24 08:22:46 -0400

    compute color and size for syn data

--- utils/xview_synthetic_util/rc_difference_analyze.py
+++ utils/xview_synthetic_util/rc_difference_analyze.py
@@ -11,6 +11,8 @@ import sys
 sys.path.append('/data/users/yang/code/yxu-yolov3-xview/')
 import argparse
 from utils.xview_synthetic_util import preprocess_xview_syn_data_distribution as pps
+import cv2
+
 
 def compare_overlap(file1, file2, save_file):
 
@@ -42,16 +44,16 @@ def compare_overlap(file1, file2, save_file):
     plt.savefig(save_file)
 
 
-def compare_images(rgb_dir1, xlbl1, rgb_dir2, xlbl2):
+def compare_images(rgb_dir1, xlbl1, rgb_dir2, xlbl2, save_dir):
     step = syn_args.tile_size * syn_args.resolution
 
-    save_dir = os.path.join(syn_args.cat_sample_dir, 'compare_rgb_same')
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     rgb_dir1 = os.path.join(rgb_dir1, 'color_all_images_step{}'.format(step))
     rgb_dir2 = os.path.join(rgb_dir2, 'color_all_images_step{}'.format(step))
-
+    print('dirs ', rgb_dir2)
     tx_image_files = np.sort(glob(os.path.join(rgb_dir1, '*.png')))
+    print('len ', len(tx_image_files))
     tx_img_names = [os.path.basename(f) for f in tx_image_files]
 
     for ix in range(len(tx_image_files)):
@@ -77,6 +79,79 @@ def compare_images(rgb_dir1, xlbl1, rgb_dir2, xlbl2):
         # plt.show()
         # plt.show()
         # exit(0)
+
+def augment_hsv(img, hgain=0.5, sgain=0.5, vgain=0.5):
+    randx = np.random.uniform(-1, 1, 3) * np.array([hgain, sgain, vgain])
+    x = (randx + 1).astype(np.float32)  # random gains
+    img_hsv = (cv2.cvtColor(img, cv2.COLOR_BGR2HSV) * x.reshape((1, 1, 3))).clip(None, 255).astype(np.uint8)
+    # cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR, dst=img)  # no return needed
+    return cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR), randx
+
+
+def compare_images_aug_ornot(img, ori_xlbl,  aug_img, aug_xlbl, save_file):
+    fig, axs = plt.subplots(1, 3, figsize=(15, 8), sharex=True, sharey=True)
+    axs[0].imshow(img)
+    axs[0].set_xlabel(ori_xlbl)
+    axs[0].set_title(os.path.basename(save_file))
+    axs[1].imshow(aug_img)
+    axs[1].set_xlabel(aug_xlbl)
+    axs[2].imshow(img - aug_img)
+    axs[2].set_xlabel('left - right')
+    plt.tight_layout()
+    plt.show()
+    fig.savefig(save_file)
+
+
+def compare_RGB_channels(rgb_dir1, xlbl1, rgb_dir2, xlbl2, save_dir):
+    step = syn_args.tile_size * syn_args.resolution
+
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    rgb_dir1 = os.path.join(rgb_dir1, 'color_all_images_step{}'.format(step))
+    rgb_dir2 = os.path.join(rgb_dir2, 'color_all_images_step{}'.format(step))
+    print('dirs ', rgb_dir2)
+    tx_image_files = np.sort(glob(os.path.join(rgb_dir1, '*.png')))
+    print('len ', len(tx_image_files))
+    tx_img_names = [os.path.basename(f) for f in tx_image_files]
+
+    for ix in range(len(tx_image_files)):
+    # for ix in range(10):
+        file1 = tx_image_files[ix]
+        file2 = os.path.join(rgb_dir2, tx_img_names[ix])
+
+        img_file1 = io.imread(file1)
+        img_file2 = io.imread(file2)
+
+        diff_img = img_file1 - img_file2
+
+        fig, axs = plt.subplots(2, 3, figsize=(15, 8))
+
+        axs[0, 0].imshow(img_file1)
+        axs[0, 0].set_title(tx_img_names[ix])
+        axs[0, 0].set_xlabel(xlbl1)
+        axs[0, 1].imshow(img_file2)
+        axs[0, 1].set_title(tx_img_names[ix])
+        axs[0, 1].set_xlabel(xlbl2)
+        axs[0, 2].imshow(diff_img)
+        axs[0, 2].set_xlabel('left - right')
+        # axs[1, 0].imshow(img_file1[:, :, 0] - img_file2[:, :, 0])
+        axs[1, 0].hist(diff_img[:, :, 0].ravel(), range=[1, 255], bins=10)
+        axs[1, 0].set_xlabel('left - right R')
+        axs[1, 0].set_ylim(0, 300)
+        # axs[1, 1].imshow(img_file1[:, :, 1] - img_file2[:, :, 1])
+        axs[1, 1].hist(diff_img[:, :, 1].ravel(), range=[1, 255], bins=10)
+        axs[1, 1].set_ylim(0, 300)
+        axs[1, 1].set_xlabel('left - right G')
+        # axs[1, 2].imshow(img_file1[:, :, 2] - img_file2[:, :, 2])
+        axs[1, 2].hist(diff_img[:, :, 2].ravel(), range=[1, 255], bins=10)
+        axs[1, 2].set_ylim(0, 300)
+        axs[1, 2].set_xlabel('left - right B')
+        plt.tight_layout()
+        fig.savefig(os.path.join(save_dir, tx_img_names[ix]))
+        # plt.show()
+        plt.cla()
+        # exit(0)
+
 
 def get_part_syn_args():
     parser = argparse.ArgumentParser()
@@ -120,11 +195,116 @@ def get_part_syn_args():
 
 if __name__ == "__main__":
     syn_args = get_part_syn_args()
-    folder1 = 'syn_xview_bkg_xbw_xcolor_xbkg_unif_shdw_split_scatter_gauss_rndsolar_bxmuller_promu_size_bias0.06_RC5_v23'
-    xlbl1  = folder1[folder1.find('promu_size'):]
-    rgb_dir1 = syn_args.syn_images_save_dir.format(folder1)
-    folder2 = 'syn_xview_bkg_xbw_xbkg_unif_shdw_split_scatter_gauss_rndsolar_ssig0.06_bxmuller_color_bias0_RC5_v11'
-    xlbl2  = folder2[folder2.find('ssig'):]
-    rgb_dir2 = syn_args.syn_images_save_dir.format(folder2)
-    compare_images(rgb_dir1, xlbl1,  rgb_dir2, xlbl2)
+    # folder1 = 'syn_xview_bkg_xbw_xcolor_xbkg_unif_shdw_split_scatter_gauss_rndsolar_bxmuller_promu_size_bias0.06_RC5_v23'
+    # xlbl1  = folder1[folder1.find('promu_size'):]
+    # rgb_dir1 = syn_args.syn_images_save_dir.format(folder1)
+    # folder2 = 'syn_xview_bkg_xbw_xbkg_unif_shdw_split_scatter_gauss_rndsolar_ssig0.06_bxmuller_color_bias0_RC5_v11'
+    # xlbl2  = folder2[folder2.find('ssig'):]
+    # rgb_dir2 = syn_args.syn_images_save_dir.format(folder2)
+    #  save_dir = os.path.join(syn_args.cat_sample_dir, 'compare_rgb_same')
+    # compare_images(rgb_dir1, xlbl1,  rgb_dir2, xlbl2, save_dir)
+
+
+    '''
+    adjust hsv 
+    '''
+    # px_thres = 23
+    # whr_thres = 3
+    # folder = 'syn_xview_bkg_xbw_xbkg_unif_shdw_split_scatter_gauss_rndsolar_ssig0.09_bxmuller_color_bias0_RC4_v11'
+    # img_dir = os.path.join(syn_args.syn_images_save_dir.format(folder), 'color_all_images_step182.4')
+    # save_dir = os.path.join(syn_args.syn_images_save_dir.format(folder), 'aughsv')
+    # img_files = glob(os.path.join(img_dir, '*.png'))
+    # # h = 0.0138
+    # # s = 0.678
+    # # v = 0.36
+    # h = 0.0138
+    # s = 0.339
+    # v = 0.18
+    # np.random.seed(2)
+    # for img_f in img_files:
+    #     img_name = os.path.basename(img_f)
+    #     img = io.imread(img_f)
+    #     ori_xlbl = 'original'
+    #     aug_img, hsv= augment_hsv(img, h, s, v)
+    #     aug_xlbl = 'agument h={:.3f} s={:.3f} v={:.3f}'.format(hsv[0], hsv[1], hsv[2])
+    #     save_file = os.path.join(save_dir, img_name)
+    #     compare_images_aug_ornot(img, ori_xlbl,  aug_img, aug_xlbl, save_file)
+
+    '''
+    compare images
+    plot histogram
+    '''
+    # img_dir = '/media/lab/Yang/data/comp_syn/'
+
+    # size0_folder = 'syn_xview_bkg_xbw_xbkg_unif_mig21_shdw_split_scatter_gauss_rndsolar_promu_size_bias0_RC1_v40'
+    # xlbl1 = 'size0'
+    # c0_folder = 'syn_xview_bkg_xbw_xbkg_unif_mig21_shdw_split_scatter_gauss_rndsolar_ssig0_color_bias0_RC1_v50'
+    # csig_folder = 'syn_xview_bkg_xbw_xbkg_unif_mig21_shdw_split_scatter_gauss_rndsolar_ssig0_color_bias45_RC1_v53'
+    # xlbl = 'size0_csig45'
+    # save_dir= os.path.join(syn_args.cat_sample_dir, 'compare_size0_csig0_RC1')
+
+    # size0_folder = 'syn_xview_bkg_xbw_xbkg_unif_shdw_split_scatter_gauss_rndsolar_promu_size_bias0.09_RC4_v43'
+    # xlbl1 = 'size0.09'
+    # c0_folder = 'syn_xview_bkg_xbw_xbkg_unif_shdw_split_scatter_gauss_rndsolar_ssig0.09_color_bias0_RC4_v50'
+    # xlbl2 = 'size0.09_csig0'
+    # csig_folder = 'syn_xview_bkg_xbw_xbkg_unif_shdw_split_scatter_gauss_rndsolar_ssig0.09_color_bias60_RC4_v54'
+    # xlbl2 = 'size0.09_csig60'
+    # save_dir= os.path.join(syn_args.cat_sample_dir, 'compare_RC4', xlbl2)
+
+    # size0_folder = 'syn_xview_bkg_xbw_xbkg_unif_shdw_split_scatter_gauss_rndsolar_promu_size_bias0.03_RC5_v41'
+    # xlbl1 = 'size0.03'
+    # c0_folder = 'syn_xview_bkg_xbw_xbkg_unif_shdw_split_scatter_gauss_rndsolar_ssig0.03_color_bias0_RC5_v50'
+    # xlbl2 = 'size0.03_csig0'
+    # save_dir= os.path.join(syn_args.cat_sample_dir, 'compare_RC4', xlbl2)
+    # compare_RGB_channels(os.path.join(img_dir, size0_folder), xlbl1, os.path.join(img_dir, c0_folder), xlbl2, save_dir)
+    # compare_RGB_channels(os.path.join(img_dir, size0_folder), xlbl1, os.path.join(img_dir, csig_folder), xlbl2, save_dir)
+
+    # save_dir= os.path.join(syn_args.cat_sample_dir, 'compare_size0_csig45_RC1')
+    # compare_RGB_channels(os.path.join(img_dir, c0_folder), 'size0_csig0', os.path.join(img_dir, csig_folder), xlbl2, save_dir)
+
+    '''
+    histogram of RGB
+    '''
+    img_dir = '/media/lab/Yang/data/comp_syn/'
+    folder = 'syn_xview_bkg_xbw_xbkg_unif_shdw_split_scatter_gauss_rndsolar_ssig0.09_color_bias0_RC4_v50'
+    files = np.sort(glob(os.path.join(img_dir, folder, 'color_all_images_step182.4', '*.png')))
+    folder2 = 'syn_xview_bkg_xbw_xbkg_unif_shdw_split_scatter_gauss_rndsolar_ssig0.09_color_bias15_RC4_v51'
+    files2 = np.sort(glob(os.path.join(img_dir, folder2, 'color_all_images_step182.4', '*.png')))
+    save_dir = os.path.join(syn_args.cat_sample_dir, 'compare_RC4', 'comp_0_15')
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    for ix, f in enumerate(files):
+        img = io.imread(f)
+        img2 = io.imread(files2[ix])
+        fig, axs = plt.subplots(2, 3, figsize=(15, 8))
+        axs[0, 0].imshow(img)
+        axs[0, 0].set_title(os.path.basename(f))
+        # axs[0, 0].set_xlabel('s')
+        axs[0, 1].imshow(img2)
+        axs[0, 2].imshow(img-img2)
+        axs[1, 0].hist(img[:, :, 0].ravel(), bins = 30)
+        axs[1, 1].hist(img[:, :, 1].ravel(), bins = 30)
+        axs[1, 2].hist(img[:, :, 2].ravel(), bins = 30)
+        axs[1, 0].hist(img2[:, :, 0].ravel(), bins = 30)
+        axs[1, 1].hist(img2[:, :, 1].ravel(), bins = 30)
+        axs[1, 2].hist(img2[:, :, 2].ravel(), bins = 30)
+        plt.show()
+
+        # plt.savefig(os.path.joint(save_dir, os.path.basename(f)))
+        # fig, axs = plt.subplots(2, 3, figsize=(15, 8))
+        # axs[0, 0].hist(img[:, :, 0].ravel(), bins = 30)
+        # axs[0, 1].hist(img[:, :, 1].ravel(), bins = 30)
+        # axs[0, 2].hist(img[:, :, 2].ravel(), bins = 30)
+        # axs[0, 0].hist(img2[:, :, 0].ravel(), bins = 30)
+        # axs[0, 1].hist(img2[:, :, 1].ravel(), bins = 30)
+        # axs[0, 2].hist(img2[:, :, 2].ravel(), bins = 30)
+        # axs[1, 0].hist(img2[:, :, 0].ravel(), bins = 30)
+        # axs[1, 1].hist(img2[:, :, 1].ravel(), bins = 30)
+        # axs[1, 2].hist(img2[:, :, 2].ravel(), bins = 30)
+        # plt.show()
+
+
+
+
+
 
--- utils/xview_synthetic_util/size_mean_std.py
+++ utils/xview_synthetic_util/size_mean_std.py
@@ -38,109 +38,111 @@ def compute_size():
         df_rgb.to_excel(writer, sheet_name='rc{}'.format(rare_cls), index=False)
 
 
-def generate_normal(mu, sigma, size):
-    np.random.seed(1)
-    u = np.random.random(size=size)
+def generate_normal(sd, mu, sigma, size):
+    np.random.seed(sd)
+    u_list = np.random.random(size=size)
     v_list = np.random.random(size=size)
     gs = []
-    for v in v_list:
-    # z1 = np.sqrt(-2 * np.log(u)) * np.sin(2 * np.pi * v)
+    gsstr = ""
+    for ix in range(size):
+        u = u_list[ix]
+        v = v_list[ix]
+        # z1 = np.sqrt(-2 * np.log(u)) * np.sin(2 * np.pi * v)
         z2 = np.sqrt(-2 * np.log(u)) * np.cos(2 * np.pi * v)
-
+        print('u', u, 'v', v, 'z2', z2)
     # x1 = mu + z1 * sigma
         x2 = mu + z2 * sigma
         gs.append(x2)
+        gsstr += '{:.2f};'.format(x2)
 
-    return gs
+    return gs, gsstr
 
 def gaussian_disribution_of_size(mu, ssig_list, rare_cls, num_aft):
     np.random.seed(1)
     mu = np.array(mu)
-    save_dir = '/media/lab/Yang/code/yolov3/result_output/1_cls/RC_size/'
+    save_dir = '../../result_output/RC_size/'
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
-    df_size = pd.DataFrame(columns=['Version', 'mean_body', 'mean_wing', 'size_sqsigma'], index=None)
+    # df_size = pd.DataFrame(columns=['Version', 'mean_body', 'mean_wing', 'size_sqsigma'], index=None)
     f_txt = open(os.path.join(save_dir, 'RC{}_size.txt'.format(rare_cls)), 'w')
+    gs = []
     for ix, ssig in enumerate(ssig_list):
-        diag = np.diag([ssig, ssig])
+        diag = ssig * np.diag(mu)
         gs = np.random.multivariate_normal(mu, diag, num_aft)
         # plt.hist(gs, 30)
+        # a = np.random.choice(gs[:, 0], 450*2)
+        # print("mean, std", np.mean(a), np.std(a))
+        # plt.hist(a, 30)
         # plt.show()
         body_str = ''
         wing_str = ''
         for jx in range(gs.shape[0]):
-            body_str += '{:.3f};'.format(gs[jx, 0])
-            wing_str += '{:.3f};'.format(gs[jx, 1])
+            body_str += '{:.2f};'.format(gs[jx, 0])
+            wing_str += '{:.2f};'.format(gs[jx, 1])
 
-        f_txt.write('body{}: "{}"\n\n'.format(ix, body_str))
-        f_txt.write('wing{}: "{}"\n\n'.format(ix, wing_str))
-        df_size = df_size.append({'Version':ix, 'mean_body':mu[0], 'mean_wing':mu[1], 'size_sqsigma':ssig}, ignore_index=True)
+        f_txt.write('@Hidden\nattr body{}= "{}"\n\n'.format(int(ssig*100), body_str))
+        f_txt.write('@Hidden\nattr wing{}= "{}"\n\n'.format(int(ssig*100), wing_str))
+        # df_size = df_size.append({'Version':ix, 'mean_body':mu[0], 'mean_wing':mu[1], 'size_sqsigma':ssig}, ignore_index=True)
     f_txt.close()
-    if rare_cls == 1:
-        mode = 'w'
-    else:
-        mode = 'a'
-    with pd.ExcelWriter(os.path.join(save_dir, 'rare_class_size_mean_sigma.xlsx'), mode=mode) as writer:
-        df_size.to_excel(writer, sheet_name='rc{}'.format(rare_cls), index=False)
+    # if rare_cls == 1:
+    #     mode = 'w'
+    # else:
+    #     mode = 'a'
+    # with pd.ExcelWriter(os.path.join(save_dir, 'rare_class_size_mean_sigma.xlsx'), mode=mode) as writer:
+    #     df_size.to_excel(writer, sheet_name='rc{}'.format(rare_cls), index=False)
+    # df_size.tocsv(os.path.join(save_dir, 'RC{}_size_mean_sigma.csv'.format(rare_cls)), index=False)
 
 
 if __name__ == '__main__':
     ## rc1
-    # base_ix = 43
-    # size_range = [x for x in range(-1, 7)]
-    # num_aft = 450*6*3
-    # rare_cls = 1
-    # mu = [13, 7]
-    # ssig_list = [0, 0.03, 0.06, 0.09, 0.12]
-    # gaussian_disribution_of_size(mu, ssig_list, rare_cls, num_aft)
+    num_aft = int(450*7*3)
+    rare_cls = 1
+    mu = [13, 7]
+    ssig_list = [0, 0.03, 0.06, 0.09, 0.12]
+    gaussian_disribution_of_size(mu, ssig_list, rare_cls, num_aft)
 
-    #
-    # ## rc2
-    # # base_ix = 23
-    # # size_range = [x for x in range(-3, 5)]
-    # num_aft = 450*6*3
+    # # ## rc2
+    # num_aft = int(450*7*3)
     # rare_cls = 2
     # mu = [38.3, 33]
     # ssig_list = [0, 0.03, 0.06, 0.09, 0.12]
     # gaussian_disribution_of_size(mu, ssig_list, rare_cls, num_aft)
     #
     # ## rc3
-    # # rc_low = 22
-    # # rc_high = 28
-    # # base_ix = 23
-    # # size_range = [x for x in range(-2, 6)]
-    # num_aft = 450*6*3
+    # num_aft = int(450*7*3)
     # rare_cls = 3
     # mu = [25.7, 25.5]
     # ssig_list = [0, 0.03, 0.06, 0.09, 0.12]
     # gaussian_disribution_of_size(mu, ssig_list, rare_cls, num_aft)
     #
     # ## rc4
-    # # rc_low = 30
-    # # rc_high = 32
-    # # base_ix = 23
-    # # size_range = [x for x in range(-3, 5)]
-    # num_aft = 450*6*4
+    # num_aft = int(450*7*3)
     # rare_cls = 4
     # mu = [31, 39]
     # ssig_list = [0, 0.03, 0.06, 0.09, 0.12]
     # gaussian_disribution_of_size(mu, ssig_list, rare_cls, num_aft)
     #
     # ## rc5
-    # # rc_low = 7.5
-    # # rc_high = 10
-    # # base_ix = 23
-    # # size_range = [x for x in range(0, 8)]
-    # num_aft = 450*6*3
+    # num_aft = int(450*7*3)
     # rare_cls = 5
     # mu = [8.3, 19.9]
     # ssig_list = [0, 0.03, 0.06, 0.09, 0.12]
     # gaussian_disribution_of_size(mu, ssig_list, rare_cls, num_aft)
 
 
-    mu = 13
-    ssig = 13*0.03
-    gs = generate_normal(mu, ssig, size=450*24)
-    print('gs ', len(gs), np.mean(gs), np.std(gs))
+    # mu = 25.7
+    # ssig = mu*0.12
+    # print('ssig', ssig)
+    # all_gs = []
+    # all_gsstr = ""
+    # np.random.seed(1)
+    # seeds = np.random.choice(5000, 450)
+    # for i in range(450):
+    #     sd = seeds[i]
+    #     gs, gsstr = generate_normal(sd, mu, ssig, size=7*2)
+    #     all_gs.extend(gs)
+    #     all_gsstr += gsstr
+    # print('gs min {:.3f}  max {:.3f}'.format(np.min(all_gs), np.max(all_gs)))
+    # print('gs len {} mean {:.3f} std {:.3f}'.format(len(all_gs), np.mean(all_gs), np.std(all_gs)))
     # plt.hist(np.array(gs), bins=10, density=True)
     # plt.show()
